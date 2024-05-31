0cc34032fdf386295d657e7f085c0a81c2a86c3a
Vhmit <viktor.9630@protonmail.com>
2024-05-31 10:10:32 -0300

    channel: Remove fingerprint from makefile
    * It leaves our cts simply broken.
    
    Change-Id: I47f593e403459939f6d47edfa30d9a8ba6b2b7dc

--- lineage_channel.mk
+++ lineage_channel.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-BUILD_FINGERPRINT := "motorola/channel_retail/channel:10/QPYS30.85-23-8-2/c00f57:user/release-keys"
+#BUILD_FINGERPRINT := "motorola/channel_retail/channel:10/QPYS30.85-23-8-2/c00f57:user/release-keys"
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := channel
 PRODUCT_MANUFACTURER := motorola
