Bomb-Projects/device_motorola_channel

channel: Dynamic Partitions

BoardConfig.mk
@@ -149,18 +149,39 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/
 # Memory Config
 MALLOC_SVELTE := true
 
-# Partitions
+# MetaData
+BOARD_USES_METADATA_PARTITION := true
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+
+# Partitions - Common
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2885681152
-BOARD_VENDORIMAGE_PARTITION_SIZE := 603979776
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
+TARGET_COPY_OUT_SYSTEM := system
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - FileSystem
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - Dynamic
+BOARD_SUPER_PARTITION_GROUPS := moto_dynamic_partitions
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := system oem vendor
+BOARD_MOTO_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor
+
+# Partitions - Sizes
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 2466250752
+BOARD_SUPER_PARTITION_OEM_DEVICE_SIZE := 285212672
+BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 335544320
+
+BOARD_SUPER_PARTITION_SIZE := 3087007744
+BOARD_MOTO_DYNAMIC_PARTITIONS_SIZE := 3078619136 # BOARD_SUPER_PARTITION_SIZE -8MB
+
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 471859200
 
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/fsg:/fsg

crdroidandroid/android_device_motorola_channel-

Change-Id: I634ee5864f5cf448986c6ec099f691b53737b9fa

BoardConfig.mk
@@ -15,7 +15,9 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
     boot \
     dtbo \
+    product \
     system \
+    system_ext \
     vendor
 
 # Audio
@@ -149,18 +151,49 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/
 # Memory Config
 MALLOC_SVELTE := true
 
-# Partitions
+# MetaData
+BOARD_USES_METADATA_PARTITION := true
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+
+# Partitions - Common
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2466250752
-BOARD_VENDORIMAGE_PARTITION_SIZE := 335544320
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM := system
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - FS
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_COPY_OUT_VENDOR := vendor
+
+# Partitions - Dynamic
+BOARD_SUPER_PARTITION_GROUPS := moto_dynamic_partitions
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := oem system vendor
+BOARD_MOTO_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext vendor
+
+# Partitions - Sizes
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 2466250752
+BOARD_SUPER_PARTITION_OEM_DEVICE_SIZE := 285212672
+BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 335544320
+
+BOARD_SUPER_PARTITION_SIZE := 3087007744
+BOARD_MOTO_DYNAMIC_PARTITIONS_SIZE := 3082813440 # BOARD_SUPER_PARTITION_SIZE -4MB
+
+# Partitions - Reserved Sizes
+ifneq ($(WITH_GMS),true)
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 137264995
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 392265885
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
+endif
 
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/fsg:/fsg